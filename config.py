f51c2f52721f35ff6f2194a409732f09e7ae3576
yuosefm15 <105274783+yuosefm15@users.noreply.github.com>
2022-05-12 06:24:23 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+MONGODB_URL = getenv("MONGODB_URL")
+OWNER_NAME = getenv("OWNER_NAME")
+ALIVE_NAME = getenv("ALIVE_NAME")
+BOT_PHOTO = getenv("BOT_PHOTO")
+DEV_PHOTO = getenv("DEV_PHOTO")
+DEV_NAME = getenv("DEV_NAME")
+BOT_USERNAME = getenv("BOT_USERNAME")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO")
+UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "master")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "us7a5")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "us7a5")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1257421053").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . ت م ا غ س ش ك S").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://te.legra.ph/file/491ecf34485c96a72572d.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "900"))
+UPSTREAM_REPO = getenv("https://github.com/C2BoT/music")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
+IMG_5 = getenv("IMG_5", "https://telegra.ph/file/d08d6474628be7571f013.png")
---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+from pytgcalls import idle
+from driver.veez import call_py, bot, user
+
+
+async def start_bot():
+    await bot.start()
+    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
+    await call_py.start()
+    print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
+    await user.join_chat("us7a5")
+    await user.join_chat("TE_NetFlix")
+    await user.join_chat("us7a5")
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
